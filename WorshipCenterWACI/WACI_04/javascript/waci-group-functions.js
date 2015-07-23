7e313cec497a84cab5b73da0e5072f1540520064
Jared Brees <j8t8b@yahoo.com>
2015-07-23 15:35:04 -0500

    bug fixes and more
    working on getting this code up-to-spec/feature parity, versus the
    current codebase.

---
+++ WorshipCenterWACI/WACI_04/javascript/waci-group-functions.js
@@ -0,0 +1,97 @@
+/*
+	This file contains the "group" functions, like Master ON/OFF,
+	Plasmas ON/OFF, et cetera.
+*/
+
+
+
+function vcPlasmasON()
+{
+	window.setTimeout("waciBox(wc, vcPlasma01, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma02, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma03, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma04, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma05, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma06, nope, power_on);",1000);
+}
+
+function vcPlasmasOFF()
+{
+	window.setTimeout("waciBox(wc, vcPlasma01, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma02, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma03, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma04, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma05, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, vcPlasma06, nope, power_off);",1000);
+}
+
+
+function allPlasmasON()
+{
+	window.setTimeout("vcPlasmasON();",1000);
+	window.setTimeout("waciBox(wc, footballTV, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, prayerTVs, nope, power_on);",1000);  
+	window.setTimeout("waciBox(wc, gsPlasmas, nope, power_on);",1000);  
+	window.setTimeout("waciBox(wc, entryPlasmas, nope, power_on);",1000);  
+}
+
+function allPlasmasOFF()
+{
+	window.setTimeout("vcPlasmasOFF();",1000);
+	window.setTimeout("waciBox(wc, footballTV, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, prayerTVs, nope, power_off);",1000);  
+	window.setTimeout("waciBox(wc, gsPlasmas, nope, power_off);",1000);  
+	window.setTimeout("waciBox(wc, entryPlasmas, nope, power_off);",1000);  
+}
+
+
+function projectorsON()
+{
+	window.setTimeout("waciBox(wc, centerCenter, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, centerLeft, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, centerRight, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, mainLeft, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, mainRight, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, foldbackLeft, nope, power_on);",1000);
+	window.setTimeout("waciBox(wc, foldbackRight, nope, power_on);",1000);	
+	window.setTimeout("waciBox(wc, ***PROJECTOR8***, nope, power_on);",1000);	
+}
+
+function projectorsOFF()
+{
+	window.setTimeout("waciBox(wc, centerCenter, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, centerLeft, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, centerRight, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, mainLeft, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, mainRight, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, foldbackLeft, nope, power_off);",1000);
+	window.setTimeout("waciBox(wc, foldbackRight, nope, power_off);",1000);	
+	window.setTimeout("waciBox(wc, ***PROJECTOR8***, nope, power_off);",1000);	
+}
+
+
+function masterWCOn()
+{
+	var d = new Date().getDay();
+	if (d == "0" || d == "6") 
+	{
+		window.setTimeout("projectorsON();",1000);
+		window.setTimeout("vcPlasmasON();",1000);
+		window.setTimeout("waciBox(wc, footballTV, nope, power_on);",1000);
+		window.setTimeout("waciBox(wc, prayerTVs, nope, power_on);",1000);  
+	}
+	else
+	{
+		window.setTimeout("projectorsON();",1000);
+		window.setTimeout("vcPlasmasON();",1000);
+		window.setTimeout("waciBox(wc, prayerTVs, nope, power_on);",1000);  
+	}
+}
+
+function masterWCOff()
+{
+		window.setTimeout("projectorsON();",1000);
+		window.setTimeout("vcPlasmasON();",1000);
+		window.setTimeout("waciBox(wc, footballTV, nope, power_off);",1000);
+		window.setTimeout("waciBox(wc, prayerTVs, nope, power_off);",1000);  
+}
